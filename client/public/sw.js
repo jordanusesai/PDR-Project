9355575d2df4a94e41bfdc4c390dbc2295df3d73
jordanusesai <jordanusesai@gmail.com>
2026-02-13 13:48:09 +0000

    Added PWA (Progressive Web App) function

---
+++ client/public/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'pdr-split-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json',
+  '/favicon.ico',
+  '/favicon-192.png',
+  '/favicon-512.png',
+  '/offline.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        
+        // If offline and requesting HTML, serve offline page
+        if (event.request.destination === 'document' && !navigator.onLine) {
+          return caches.match('/offline.html');
+        }
+        
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Delete old caches
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
